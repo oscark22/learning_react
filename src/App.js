8a5c50b9fd90da4915b178f3741c35c941199121
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-10-10 15:46:32 -0700

    new functions for tasks

--- src/App.js
+++ src/App.js
@@ -23,11 +23,25 @@ function App() {
       reminder: false,
     },
   ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)))
+  }
   
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? 
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
+      : (
+        '@No tasks to show...'
+      )}
     </div>
   );
 }
